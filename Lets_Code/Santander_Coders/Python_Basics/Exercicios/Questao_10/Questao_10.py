32018b6c6eef0f15c537aa1b978d0775ffa92748
Jonathas Gabriel Davide Tenorio <92163170+JonathasTenorio@users.noreply.github.com>
2022-09-14 09:25:39 -0300

    Resolução final do exercício 10

--- Lets_Code/Santander_Coders/Python_Basics/Exercicios/Questao_10/Questao_10.py
+++ Lets_Code/Santander_Coders/Python_Basics/Exercicios/Questao_10/Questao_10.py
@@ -1,40 +1,51 @@
-print("""Este programa irá verificar se a composição possui a compassagem correta.
+tru = True
+while tru:
+    print("""\nEste programa irá verificar se a composição possui a compassagem correta.
     Exemplo de input: /W/HH/QQQQ/
-    """)
-notas = {
-        'W': 1,
-        'H': 1/2,
-        'Q': 1/4,
-        'E': 1/8,
-        'S': 1/16,
-        'T': 1/32,
-        'X': 1/64
-        }
+            """)
+    notas = {
+                'W': 1,
+                'H': 1/2,
+                'Q': 1/4,
+                'E': 1/8,
+                'S': 1/16,
+                'T': 1/32,
+                'X': 1/64
+                }
 
-composicao = str.upper(input("Insira a composição\n"))
-print(composicao) #Apagar depois
-composicao = composicao.split('/')
-print(composicao) #Apagar Depois
+    composicao = str.upper(input("Insira a composição\n"))
+    composicao = composicao.split('/')
 
-for element in range(len(composicao)): #Rever posteriormente, provável erro de lógica entre iteração e método.
-    if '' == composicao[element-1]:
-        composicao.remove('')
-print(composicao) # Apagar depois
-somador = 0
-counter = 0
-errors = []
-acertos = 0
-#while counter < len(composicao):
-for element in composicao:
-    if somador < 1:
-        print(f'Estamos no elemento: {element}')
+    for element in range(len(composicao)):
+        if '' == composicao[element-1]:
+            composicao.remove('')
+    errors = []
+    acertos = 0
+    for element in composicao:
+        somador = 0
         for note in element:
-            print(f'Esta é a nota: {note} do elemento {element}')
-            for key in notas:
-                print(f'Esta é a chave: {key}')
-                if note == key:
-                    print('Igual')
-                    somador = somador + notas[key]
-                    break
-                #else:
-print(somador)
+            if somador > 1:
+                break
+            else:
+                for key in notas:
+                    if note == key:
+                        somador = somador + notas[key]
+                        break
+        if somador != 1:
+            errors.append(element)
+        else:
+            acertos = acertos + 1
+
+    print(f'Qtd. de Corretos: {acertos}')
+    print(f'Incorretos: {errors}')
+    slc = '0'
+    while slc != '1' or slc != '2':
+        slc = str(input("""Deseja realizar uma nova consulta ?
+        1) -- SIM
+        2) -- NÃO
+        """))
+        if slc == '2':
+            tru = False
+            break
+        else:
+            break
