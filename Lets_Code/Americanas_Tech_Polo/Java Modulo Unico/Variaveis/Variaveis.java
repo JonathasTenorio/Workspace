9640936b11ae1f10d98c8ddf0f816f6ac6f663fb
Jonathas Gabriel Davide Tenorio <92163170+JonathasTenorio@users.noreply.github.com>
2022-09-16 09:42:08 -0300

    Upload PDF's

---
+++ Lets_Code/Americanas_Tech_Polo/Java Modulo Unico/Variaveis/Variaveis.java
@@ -0,0 +1,20 @@
+public class Variaveis {
+    public static void main(String[] args) {
+        String nome;
+        nome = "Jhonny";
+        nome = "Tenorio";
+        System.out.println("Olá " + nome);
+
+        int a;
+        int b = 2;
+        a = 3;
+        int soma = a + b;
+        int sub = a - b;
+        int multi = a * b;
+        float divi = (float) a / b;
+        System.out.println(soma);
+        System.out.println(sub);
+        System.out.println(multi);
+        System.out.println(divi);
+    }
+}
