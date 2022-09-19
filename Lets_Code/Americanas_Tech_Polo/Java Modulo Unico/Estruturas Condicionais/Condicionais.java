f3bb5cb4ec3121db380cfbbd5e022114369a9400
Jonathas Gabriel Davide Tenorio <92163170+JonathasTenorio@users.noreply.github.com>
2022-09-19 09:14:45 -0300

    Upload Java Basics

---
+++ Lets_Code/Americanas_Tech_Polo/Java Modulo Unico/Estruturas Condicionais/Condicionais.java
@@ -0,0 +1,37 @@
+public class Condicionais {
+    public static void main(String[] args) {
+
+        int nota = 70;
+        String graduacao;
+
+        if (nota >= 80){
+            graduacao = "A";
+        }
+        else if (nota < 80 && nota >= 70){
+            graduacao = "B";
+        }
+        else if (nota < 70 && nota >= 60){
+            graduacao = "C";
+        }
+        else if (nota < 60 && nota >= 0){
+            graduacao = "D";
+        }
+        else{
+            graduacao = "";
+        }
+
+        switch (graduacao) {
+            case "A":
+            case "B":
+                System.out.println("Aluno Aprovado!");
+                break;
+            case "C":
+            case "D":
+                System.out.println("Não Aprovado");
+                break;
+            default:
+                System.out.println("Graduação Inválida");
+                break;
+        }
+    }
+}
